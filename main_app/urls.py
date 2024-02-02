1580f986e21ad31d2782e976fb1f9f995b8d8e04
dawsonchilders <dawsonchilders@gmail.com>
2024-02-02 13:58:20 -0800

    adds restaurants model

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('restaurants/', views.restaurant_index, name='index'),
+  path('restaurants/<int:restaurant_id>/', views.restaurants_detail, name='detail'),
+  path('restaurants/create/', views.RestaurantCreate.as_view(), name='restaurants_create'),
   path('accounts/signup', views.signup, name='signup'),
+
 ]
